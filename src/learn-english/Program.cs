ac278fd4e4a2b5378db63dd67641846558b66597
Your Name <you@example.com>
2018-10-03 02:29:47 +0300

    update

--- src/learn-english/Program.cs
+++ src/learn-english/Program.cs
@@ -9,6 +9,57 @@ namespace learn_english
     {
         static void Main(string[] args)
         {
+            Dictionary<string, string> letters = new Dictionary<string, string> {
+                { "a", "ey" },
+                { "b", "bi" },
+                { "c", "si" },
+                { "d", "di" },
+                { "e", "i" },
+                { "f", "ef" },
+                { "g", "ci" },
+                { "h", "eyç" },
+                { "i", "ay" },
+                { "j", "cey" },
+                { "k", "key" },
+                { "l", "el" },
+                { "m", "em" },
+                { "n", "en" },
+                { "o", "ouğ" },
+                { "p", "pi" },
+                { "q", "küuğ" },
+                { "r", "ağr" },
+                { "s", "es" },
+                { "t", "ti" },
+                { "u", "yuğ" },
+                { "v", "vi" },
+                { "w", "dabılyuğ" },
+                { "x", "ekz" },
+                { "y", "vay" },
+                { "z", "zed" },
+            };
+            Console.Title = "learn-english";
+            Console.WriteLine("Harflerin Türkçe okunuşunu yazınız.");
+            Console.ReadKey();
+            Console.Clear();
+
+            while (true)
+            {
+                Random rand = new Random();
+                var randomEntry = letters.ElementAt(rand.Next(0, letters.Count));
+                String question = randomEntry.Key;
+                String answer = randomEntry.Value;
+
+                Console.WriteLine(question);
+                String stdin = Console.ReadLine();
+                if (stdin != answer)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Yanlış... Cevap: " + answer);
+                    Console.ResetColor();
+                    Console.ReadKey();
+                }
+                Console.Clear();
+            }
         }
     }
 }

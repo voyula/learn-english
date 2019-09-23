794ad27b4f3987cdd91970a6126384b89862abef
Your Name <you@example.com>
2019-09-23 16:28:30 +0300

    update

--- src/learn-english/Program.cs
+++ src/learn-english/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Text;
-using System.Data.SQLite;
+using System.Linq;
 
 namespace learn_english
 {
@@ -10,12 +10,15 @@ namespace learn_english
         {
             LETTERS = 1,
             NUMBERS = 2,
-            KEYWORDS = 3
+            KEYWORDS = 3,
+            TIME = 4
         }
+
         static void Main(string[] args)
         {
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
+
 
             int mode;
             Console.Title = "learn-english";
@@ -23,6 +26,7 @@ namespace learn_english
             Console.WriteLine("1 - HARFLERİN OKUNUŞU");
             Console.WriteLine("2 - SAYILAR");
             Console.WriteLine("3 - KELİMELERİN ANLAMLARI");
+            Console.WriteLine("4 - VAKİT");
             Console.WriteLine("**************************");
             int stdinMode = int.Parse(Console.ReadLine());
             switch (stdinMode)
@@ -36,45 +40,66 @@ namespace learn_english
                 case (int)Options.KEYWORDS:
                     mode = 3;
                     break;
+                case (int)Options.TIME:
+                    mode = 4;
+                    break;
                 default:
                     mode = 1;
                     break;
             }
             Console.Clear();
 
-            SQLiteConnection sqlite_conn = new SQLiteConnection("Data Source=db.sqlite3;Version=3;Compress=True;");
-            sqlite_conn.Open();
-            SQLiteCommand sqlite_cmd;
+            using (var db = new dbEntities())
+            {
+                if (mode == (int)Options.LETTERS)
+                {
+                    Console.WriteLine("Harflerin Türkçe okunuşunu yazınız.");
+                }
+                else if (mode == (int)Options.NUMBERS)
+                {
+                    Console.WriteLine("Sayıların İngilizce okunuşunu yazınız.");
+                }
+                else if (mode == (int)Options.KEYWORDS)
+                {
+                    Console.WriteLine("Harflerin Türkçe kelime anlamlarını yazınız.");
+                }
+                else if (mode == (int)Options.TIME)
+                {
+                    Console.WriteLine("Kelimelerin Türkçe kelime anlamlarını yazınız.");
+                }
+                Console.ReadKey();
+                Console.Clear();
 
-            sqlite_cmd = sqlite_conn.CreateCommand();
-
-            if (mode == 1)
-            {
-                sqlite_cmd.CommandText = "SELECT * FROM letters ORDER BY RANDOM() LIMIT 1";
-                Console.WriteLine("Harflerin Türkçe okunuşunu yazınız.");
-            }
-            else if (mode == 2)
-            {
-                sqlite_cmd.CommandText = "SELECT * FROM numbers ORDER BY RANDOM() LIMIT 1";
-                Console.WriteLine("Sayıların ingilizce okunuşunu yazınız.");
-            }
-            else if (mode == 3)
-            {
-                sqlite_cmd.CommandText = "SELECT * FROM keywords ORDER BY RANDOM() LIMIT 1";
-                Console.WriteLine("Harflerin Türkçe kelime anlamlarını yazınız.");
-            }
-            Console.ReadKey();
-            Console.Clear();
-
-            using (SQLiteDataReader reader = sqlite_cmd.ExecuteReader())
-            {
                 while (true)
                 {
-                    reader.Read();
-                    String question = (String)reader["question"];
-                    String answer = (String)reader["answer"];
+                    String question = "";
+                    String answer = "";
 
-                scope:
+                    if (mode == (int)Options.LETTERS)
+                    {
+                        IQueryable<letters> rows = db.letters.OrderBy(x => Guid.NewGuid()).Take(1);
+                        question = rows.First().question;
+                        answer = rows.First().answer;
+                    }
+                    else if (mode == (int)Options.NUMBERS)
+                    {
+                        IQueryable<numbers> rows = db.numbers.OrderBy(x => Guid.NewGuid()).Take(1);
+                        question = rows.First().question;
+                        answer = rows.First().answer;
+                    }
+                    else if (mode == (int)Options.KEYWORDS)
+                    {
+                        IQueryable<keywords> rows = db.keywords.OrderBy(x => Guid.NewGuid()).Take(1);
+                        question = rows.First().question;
+                        answer = rows.First().answer;
+                    }
+                    else if (mode == (int)Options.TIME)
+                    {
+                        IQueryable<time> rows = db.time.OrderBy(x => Guid.NewGuid()).Take(1);
+                        question = rows.First().question;
+                        answer = rows.First().answer;
+                    }
+
                     Console.WriteLine(question);
                     String stdin = Console.ReadLine().ToLower();
                     if (stdin != answer)
@@ -83,8 +108,6 @@ namespace learn_english
                         Console.WriteLine("Yanlış... Cevap: " + answer);
                         Console.ResetColor();
                         Console.ReadKey();
-                        Console.Clear();
-                        goto scope;
                     }
                     Console.Clear();
                 }
